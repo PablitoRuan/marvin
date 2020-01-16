dc1ed6764d3dad40b6d5a39751bb8a7e623f9f17
PablitoRuan <59841175+PablitoRuan@users.noreply.github.com>
2020-01-16 16:05:46 -0300

    juntar numeros

--- fase02/ex06/juntar.js
+++ fase02/ex06/juntar.js
@@ -1,6 +1,7 @@
-function juntar(num1, num2) {
-var a= num1
-var b= num2
-var c= (""+num1+num2)
-return c
+function juntar(num1,num2){
+var a = num1.toString()
+var b = num2.toString()
+var c = a + b
+var x = Number(c)
+return x
 }
