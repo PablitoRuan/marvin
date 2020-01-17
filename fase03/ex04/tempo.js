fbfd609cbeeb274c607c0ff01e5f6bb1b10be43f
PablitoRuan <59841175+PablitoRuan@users.noreply.github.com>
2020-01-17 15:23:03 -0300

    Datas e intervalos de tempo

--- fase03/ex04/tempo.js
+++ fase03/ex04/tempo.js
@@ -8,7 +8,7 @@ function tempo(data1, data2, unidade){
  var h = (f/30)/12;
  if (c === "dia")
  return Number(f.toFixed(0));
-    else if (c === "mes")
+    else if (c === "mês")
     return Number(g.toFixed(0));
     else if (c === "ano")
     return Number(h.toFixed(0));
