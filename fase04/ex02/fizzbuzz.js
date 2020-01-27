0c0a8606378c31036f52ead596d275e8835ce9ba
PablitoRuan <59841175+PablitoRuan@users.noreply.github.com>
2020-01-27 15:05:37 -0300

    FizzBuzz

--- fase04/ex02/fizzbuzz.js
+++ fase04/ex02/fizzbuzz.js
@@ -1,11 +1,14 @@
 function fizzbuzz(num1, num2){
     while (num1<=num2){
-        var str = " ";
-        if (num1 % 3 === 0) {
-            str += "Fizz";
+        var str = "";
+        if (num1 % 3 === 0 && num1 % 5 !== 0) {
+            str += " " + "Fizz";
         }
-        if (num1 % 5 === 0) {
-            str += "Buzz";
+        if (num1 % 5 === 0 && num1 % 3 !== 0) {
+            str += " " + "Buzz";
+        }
+        if (num1 % 5 === 0 && num1 % 3 === 0){
+            str += " " + "FizzBuzz"
         }
             console.log(num1+ str)
         num1++
