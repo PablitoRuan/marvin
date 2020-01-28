f5be9eb455e6413ebbc83ac7acca29cb6f0e58ae
PablitoRuan <59841175+PablitoRuan@users.noreply.github.com>
2020-01-28 15:44:41 -0300

    Upper Case e Lower Case

---
+++ fase06/ex00/meuNome.js
@@ -0,0 +1,10 @@
+function meuNome(obj){
+  var str= ""
+  var a= obj
+  var b= a.substring(0,1)
+  var c= b.toUpperCase()
+  
+  var d= a.substring(2)
+  var e= d.toLowerCase()
+  return (c+e+str)
+}
